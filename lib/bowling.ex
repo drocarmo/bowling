25a53ade6269f73dabbf5fb77d91bb983c252119
drew <drew@drewolson.org>
2014-01-20 12:08:23 -0600

    Solution.

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,2 +1,25 @@
 defmodule Bowling do
+  def score(rolls) do
+    score(rolls, 0)
+  end
+
+  defp score([a,b,c], total) when a == 10 or a + b == 10 do
+    total + a + b + c
+  end
+
+  defp score([], total) do
+    total
+  end
+
+  defp score([10,a,b|rest], total) do
+    score([a,b|rest], total + 10 + a + b)
+  end
+
+  defp score([a,b,c|rest], total) when a + b == 10 do
+    score([c|rest], total + 10 + c)
+  end
+
+  defp score([a,b|rest], total) do
+    score(rest, total + a + b)
+  end
 end
